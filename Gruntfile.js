bef66a3a7f98f82dd02c124f92012100497e1a2b
Richard Young <richard.young@utexas.edu>
2015-05-03 17:23:00 -0500

    added to package.json and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
@@ -9,12 +9,29 @@ module.exports = function(grunt) {
       unit:{
         configFile: 'karma.conf.js'
       }
+    },
+    watch: {
+      react: {
+        files: 'client/**/*.jsx',
+        tasks: ['browserify']
+      }
+    },
+    browserify: {
+      options: {
+        transform: [ require('grunt-react').browserify ]
+      },
+      client: {
+        src: ['client/**/*.jsx'],
+        dest: 'client/build.js'
+      }
     }
   });
- 
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
 
-  grunt.registerTask('default', ['jshint', 'karma']);
- 
+  grunt.registerTask('default', ['jshint', 'karma', 'browserify']);
+
 };
