94287793191b01e379230cd176380702f3ca43dd
vishalatmakuri <findvishal@gmail.com>
2015-05-04 17:19:02 -0500

    added the evets to store

--- client/stores/postStores.js
+++ client/stores/postStores.js
@@ -1,15 +1,35 @@
-// var dispatcher = require('../dispatcher/dispatcher.js');
+var Dispatcher = require('../dispatcher/dispatcher');
+var Constants = require('../constants/constants')
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
 
 var _postsList =[];
-var PostsStore =  assign({}, EventEmitter.prototype,{
-  populatestore:function(postlist){
+
+var populatestore:function(postlist){
      _postsList=posts;
-  },
+     Dispatcher.handleAction({
+        actionType: Constants.STORE_UPDATED,
+        data: _postsList
+      });
+  }
+
+var postsStore =  assign({}, EventEmitter.prototype,{
   pullposts:function(){
     return _postsList
   }
 })
 
-module.exports = PostsStore;
+
+postsStore.dispatchToken= Dispatcher.register(function(action) {
+
+  switch(action.type) {
+    case Constants.RELOAD_POSTLIST:
+      populatestore(action.data)
+      break
+    case Constants.PULL_POSTLIST:
+      pullposts();
+      break
+    default:
+      return false
+  })
+module.exports = postsStore;
