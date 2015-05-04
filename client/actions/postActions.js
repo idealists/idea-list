801049de58b6a08e66a5477c0fb0f6b3d8bf28e4
vishalatmakuri <findvishal@gmail.com>
2015-05-04 12:09:39 -0500

    action test

--- client/actions/postActions.js
+++ client/actions/postActions.js
@@ -1,19 +1,24 @@
 var Dispatcher = require('../dispatcher/dispatcher');
 var Constants = require('../constants/constants')
-
-var postsActions={
-  getitmes:function(entries){
-    Dispatcher.handleAction({
-      actionType:Constants,
-      data: entries
+var ajax = require('ajax')
+var postActions={
+  getEntries:function(query){
+    ajax.get('/posts', query, function(postsList){
+      console.log(postsList);
+      Dispatcher.handleAction({
+        actionType:Constants.RELOAD_POSTSLIST,
+        data: postsList
+      });
     })
-  }
+  },
+  newPostEntry:function(newpostentry) {
+    ajax.post('/post/create',newpostentry,function(value){
+      console.log(value)
+    })
+  },
+  deletePostEntry
+
 }
 
 
-Dispatcher.regester(function (payload){
-  var action= payload.action;
-  console.log(action)
-  switch(action.actionType){
-  }
-})
+module.exports= postActions
