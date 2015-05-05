8fa5160ecb9f796d3f3078ff774bee31321344d7
Sharky <kaydenalthen@gmail.com>
2015-05-05 16:40:53 -0500

    server.js file for local

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use('/', express.static('client'));
+app.use('/', express.static('../client'));
 app.use(bodyParser.json());
-app.get('/',function(req,res){
-  res.sendFile('index.html',{root:__dir.name+'/../client'});
-});
+// app.get('/',function(req,res){
+//   res.sendFile('index.html',{root:__dirname+'/../client'});
+// });
 require('./middleware.js')(app,express);
 
 var server = app.listen(process.env.PORT||5000, function () {
