2eb217b2a9359b40b5a3ba9402c62616725f213d
Sharky <kaydenalthen@gmail.com>
2015-05-05 16:51:17 -0500

    test for slash commands

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.use('/', express.static('../client'));
+/*For deployment*/
+app.use('/', express.static('client'));
 app.use(bodyParser.json());
-// app.get('/',function(req,res){
-//   res.sendFile('index.html',{root:__dirname+'/../client'});
-// });
+app.get('/',function(req,res){
+  res.sendFile('index.html',{root:"../client"});
+});
+/*End for deployment*/
+
+/*For local use for Kayden*/
+// app.use('/', express.static('../client'));
+// app.use(bodyParser.json());
+/*End local use for Kayden*/
+
 require('./middleware.js')(app,express);
 
 var server = app.listen(process.env.PORT||5000, function () {
@@ -17,7 +25,10 @@ var server = app.listen(process.env.PORT||5000, function () {
 
 });
 
-app.post('/api/idea', function (request, response){
-  console.log(request.body);
-  response.end();
-});
+app.post('/api/idea', function(req, res){
+  var idea = req.body.text,
+      token = req.body.token,
+      command = req.body.command
+   console.log(req.body);
+   //res.send(req.body);
+})
