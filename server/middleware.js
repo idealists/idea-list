b5c258039e1d282663558ed2bfb41f180d35bfd2
Sharky <kaydenalthen@gmail.com>
2015-05-07 16:26:18 -0500

    missing route from master branch

--- server/middleware.js
+++ server/middleware.js
@@ -5,5 +5,6 @@ module.exports = function(app, express){
   var ideas = express.Router();
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  app.use('/ideas', ideas);
   require('./ideas/idea-routes.js')(app);
 };
