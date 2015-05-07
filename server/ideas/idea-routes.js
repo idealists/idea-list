c8011c958802aa74c4df2f4d07afa6c636220206
Sharky <kaydenalthen@gmail.com>
2015-05-07 16:08:05 -0500

    fixed bug in routing

--- server/ideas/idea-routes.js
+++ server/ideas/idea-routes.js
@@ -3,7 +3,6 @@ var isAuth = require('../auth.js').isAuthenticated;
 
 module.exports = function(app){
   app.get('/', isAuth, ideaFunctions.getIdeas);
-  app.post('/create', isAuth, ideaFunctions.createIdea);
+  app.post('/api/idea', isAuth, ideaFunctions.createIdea);
   app.post('/comment', isAuth, ideaFunctions.createComment);
-  // body...
 };
