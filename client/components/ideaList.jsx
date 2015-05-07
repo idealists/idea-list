5da48efbdb81dc0f2e1463f3c6fd3addb9aa324f
Richard Young <richard.young@utexas.edu>
2015-05-07 11:51:37 -0500

    ideaList spacing

--- client/components/ideaList.jsx
+++ client/components/ideaList.jsx
@@ -7,10 +7,9 @@ var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 var RouteHandler = Router.RouteHandler;
 
-
 var IdeaList = React.createClass({
   render : function(){
-    var list = this.props.ideas.map(function(idea, index, collection){
+    var list = this.props.ideas.map(function(idea, index){
       return (
         <li key={index} >
           <h3> <Link to="ideaView">Title: {idea.title}</Link> </h3>
@@ -24,6 +23,7 @@ var IdeaList = React.createClass({
     );
   }
 });
+
 var routes = (
   <Route name='idealist' path="/" handler={IdeaList}>
     <Route name='ideaView' path=":index" handler={ideaView}/>
