f75b13354371a46b1760b70f1b14ada2ec472b3b
Gerald Tilma <gerald.tilma@gmail.com>
2015-05-09 12:30:19 -0500

    Debug routes

--- server/ideas/idea-routes.js
+++ server/ideas/idea-routes.js
@@ -1,12 +1,12 @@
 var ideaFunctions = require('./idea-functions');
 var isAuth = require('../auth').isAuthenticated;
 
-module.exports = function(app){
-  app.get('/', isAuth, ideaFunctions.getIdeas);
-  app.post('/create', isAuth, ideaFunctions.createIdea);
-  app.post('/comment', isAuth, ideaFunctions.createComment);
-  app.post('/api/idea', ideaFunctions.slackInt);
-  app.post('/api/comment', ideaFunctions.slackInt);
-  app.post('/api/downvote', ideaFunctions.slackInt);
-  app.post('/api/upvote', ideaFunctions.slackInt);
+module.exports = function(ideas){
+  ideas.get('/', isAuth, ideaFunctions.getIdeas);
+  ideas.post('/create', isAuth, ideaFunctions.createIdea);
+  ideas.post('/comment', isAuth, ideaFunctions.createComment);
+  ideas.post('/api/idea', ideaFunctions.slackInt);
+  ideas.post('/api/comment', ideaFunctions.slackInt);
+  ideas.post('/api/downvote', ideaFunctions.slackInt);
+  ideas.post('/api/upvote', ideaFunctions.slackInt);
 };
