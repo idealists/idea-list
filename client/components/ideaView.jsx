9a72d3076745af32a347c413014ebdd6d23f7011
Richard Young <richard.young@utexas.edu>
2015-05-11 14:53:26 -0500

    ideaView spacing

--- client/components/ideaView.jsx
+++ client/components/ideaView.jsx
@@ -9,27 +9,35 @@ var IdeaView = React.createClass({
     return(
       <div>
         <NavBar />
+
         <div className="page-header container">
           <div className="x-huge text-primary"> idea.title </div>
         </div>
+
         <div className="container">
           <br />
           <div className="large text-primary"> idea.body </div>
           <br />
         </div>
+
         <div className="input-group container">
           <textarea className="form-control" type='text' ref='parentComment' placeholder='body' rows="5"></textarea>
         </div>
+
         <br />
+
         <div className="text-center">
           <button className="btn btn-red btn-wide center">
             Add Comment
           </button>
         </div>
+
         <br />
+
         <div className="container">
           <CommentList />
         </div>
+
       </div>
     );
   }
