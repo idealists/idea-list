4f689aabe59779902cfb67524ffabcaeae2a1b44
Sharky <kaydenalthen@gmail.com>
2015-05-13 11:29:15 -0500

    --

--- server/ideas/slackInt.js
+++ server/ideas/slackInt.js
@@ -84,6 +84,7 @@ function slackInt (req, res){
                       .where({ active: true })
                       .sort('-updatedAt')
                       .limit(10);
+      console.log(ideas);
       res.send(JSON.stringify(ideas));
       break;
     default:
