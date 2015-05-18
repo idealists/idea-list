08374ef48feec7309f22ab55911ce5686eabb247
Sharky <kaydenalthen@gmail.com>
2015-05-18 17:50:27 -0500

    --

--- server/ideas/idea-functions.js
+++ server/ideas/idea-functions.js
@@ -225,7 +225,8 @@ function createComment (req, res) {
 
         idea.save(function(err){
           if (err) console.log('idea save error:', err);
-          
+
+          //debugging
           console.log('newComment: ', newComment);
 
           var reply = { 'text': 'Comment added to idea: ' + idea.shortId + ' / To comment on this comment, use commentId: ' + newComment.commShortId};
