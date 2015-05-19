2f9b890b1e2e59113fa0d72f5a193f409aeacd6f
Sharky <kaydenalthen@gmail.com>
2015-05-19 14:58:29 -0500

    build

--- client/build.js
+++ client/build.js
@@ -913,10 +913,9 @@ var VoteView = React.createClass({displayName: "VoteView",
   },
   sendVote: function(rating){
     var votedata = this.state.voteData;
-    console.log('this.state.userInfo' , this.state.userInfo);
     var here = this;
-    var voteInfo = {
-        voterId    : this.state.userInfo.userId,
+    var voteInfo = { 
+        voterId    : this.state.userInfo._id,
         parentId   : votedata._id,
         user_name  : this.state.userInfo.sUserName,
         voteType   : votedata.type,
