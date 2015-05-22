28723e0506e13a21e2211f117bd9c57b1951d894
Sharky <kaydenalthen@gmail.com>
2015-05-21 20:16:11 -0500

    bug fix for user images from slack reqs

--- server/ideas/idea-functions.js
+++ server/ideas/idea-functions.js
@@ -107,6 +107,8 @@ function createIdea (req, res) {
     status       : Status.OPEN
   });
 
+  console.log("INSIDE IDEA FUNCS: ", idea);
+  
   idea.save(function (err) {
     if (err) {
       return err;
